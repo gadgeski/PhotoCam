cd7f2a2f8c9f356a4269184851281744a4a79ea1
gadgeski <xtwh.ce5bil@gmail.com>
2025-07-05 19:29:37 +0900

    Add Readme.md and more

--- PhotoCam/ContentView.swift
+++ PhotoCam/ContentView.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 import PhotosUI
+    
 struct ContentView: View {
     // 撮影した写真を保持する状態変数
     @State var captureImage: UIImage? = nil
@@ -16,85 +17,102 @@ struct ContentView: View {
     @State var photoPickerSelectedImage: PhotosPickerItem? = nil
     
     var body: some View {
-        VStack {
-            // スペース追加
-            Spacer()
-            // 「カメラ起動(Touch)」ボタン
-            Button {
-                // ボタンタップアクション
-                // カメラが利用可能かチェック
-                if UIImagePickerController.isSourceTypeAvailable(.camera) {
-                    print("利用可能")
-                    // 撮影写真を初期化
-                    captureImage = nil
-                    // カメラ使用可能時isShowSheetをtrueに処理
-                    isShowSheet.toggle()
-                } else {
-                    print("利用不可")
-                }
-            } label: {
-                Text("Touch")
-                // 文字サイズ指定
-                    .font(.title)
-                // 横幅いっぱい
-                .frame(maxWidth: .infinity)
-                // 高さ50ポイント指定
-                .frame(width: 100, height: 100)
-                // 文字列センタリング指定
-                .multilineTextAlignment(.center)
-                // 背景を青色に指定
-                .background(Color.blue)
-                // 文字色を白色指定
-                .foregroundStyle(Color.white)
-                
-                // 円形に切り抜く
-                .clipShape(Circle())
+        ZStack {
+            // 背景画像を指定
+            Image(.background)
+            // リサイズ
+                .resizable()
+            // セーフエリア外まで画面表示
+                .ignoresSafeArea()
+            // アスペクト比を維持し短辺基準に収める
+                .scaledToFill()
+                let bgColor = Color.init(red: 0.3, green: 0.5, blue: 0.95)
+                let blueColor = Color.init(red: 0.15, green: 0.3, blue: 0.85, opacity: 0.5)
+            
+            VStack {
+                // スペース追加
+                Spacer()
+                // 「カメラ起動(Touch)」ボタン
+                Button {
+                    // ボタンタップアクション
+                    // カメラが利用可能かチェック
+                    if UIImagePickerController.isSourceTypeAvailable(.camera) {
+                        print("利用可能")
+                        // 撮影写真を初期化
+                        captureImage = nil
+                        // カメラ使用可能時isShowSheetをtrueに処理
+                        isShowSheet.toggle()
+                    } else {
+                        print("利用不可")
+                    }
+                } label: {
+                    Text("Touch")
+                    // 文字サイズ指定
+                        .font(.system(size: 20, weight: .semibold, design: .rounded))
+                    // 文字色を白色指定
+                        .foregroundColor(.white)
+                    // 余白追加
+                        .padding()
+                        .frame(width: 150, height: 50)
+                    // 背景指定
+                        .background(RoundedRectangle(cornerRadius: 30)
+                            .foregroundStyle(bgColor)
+                        // 上側shadow
+                            .shadow(color: .blue, radius: 10, x: -7, y: -7)
+                        // 下側shadow
+                            .shadow(color: blueColor, radius: 10, x: 7, y: 7)
+                        )
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 30)
+                                .stroke(.white, lineWidth: 0)
+                 )
+                } // 「カメラ起動ボタン」ボタン End
                 // 余白追加
-                .padding()
-            } // 「カメラ起動ボタン」ボタン End
-            // Sheet表示
-            // isPresentedで指定した状態変数がtrueの際実行
-            .sheet(isPresented: $isShowSheet) {
-                if let captureImage {
-                    // 撮影した写真がある→EffectViewを表示
-                    EffectView(isShowSheet: $isShowSheet, captureImage: captureImage)
-                } else {
-                    // UIImagePickerController(写真撮影)を表示
-                    ImagePickerView(isShowSheet: $isShowSheet, captureImage: $captureImage)
-                }
-            } // 「カメラを起動」ボタンSheet End
-            
-            // フォトライブラリーから選択
-            PhotosPicker(selection: $photoPickerSelectedImage, matching: .images, preferredItemEncoding: .automatic, photoLibrary: .shared()) {
-                // テキスト表示
-                Text("フォトライブラリーから選択")
-                // 横幅いっぱい
-                    .frame(maxWidth: .infinity)
-                // 高さ50ポイント指定
-                    .frame(height: 50)
-                // 背景を青色指定
-                    .background(Color.blue)
-                // 文字色を白色指定
-                    .foregroundStyle(Color.white)
-                // 上下左右に余白追加
                     .padding()
-            } // PhotosPicker End
-            
-            // 選択した写真情報を写真から取り出す
-            .onChange(of: photoPickerSelectedImage, initial: true, { oldValue, newValue in
-                // 選択した写真がある際
-                if let newValue {
-                    Task {
-                        // Data型で写真を取り出す
-                        if let data = try? await newValue.loadTransferable(type: Data.self) {
-                            // 写真がある際
-                            // 写真をcaptureImageに保存
-                            captureImage = UIImage(data: data)
+                // Sheet表示
+                // isPresentedで指定した状態変数がtrueの際実行
+                .sheet(isPresented: $isShowSheet) {
+                    if let captureImage {
+                        // 撮影した写真がある→EffectViewを表示
+                        EffectView(isShowSheet: $isShowSheet, captureImage: captureImage)
+                    } else {
+                        // UIImagePickerController(写真撮影)を表示
+                        ImagePickerView(isShowSheet: $isShowSheet, captureImage: $captureImage)
+                    }
+                } // 「カメラを起動」ボタンSheet End
+                
+                // フォトライブラリーから選択
+                PhotosPicker(selection: $photoPickerSelectedImage, matching: .images, preferredItemEncoding: .automatic, photoLibrary: .shared()) {
+                    // テキスト表示
+                    Text("Select to Photo Gallery")
+                    // 文字サイズ指定
+                        .font(.system(size: 20, weight: .semibold, design: .default))
+                    // 文字色を白色指定
+                        .foregroundColor(.white)
+                    // 余白追加
+                        .padding(.horizontal, 60)
+                        .padding(.vertical, 20)
+                    // 背景指定
+                        .background(Color.blue)
+                        .cornerRadius(30)
+                } // PhotosPicker End
+                
+                // 選択した写真情報を写真から取り出す
+                .onChange(of: photoPickerSelectedImage, initial: true, { oldValue, newValue in
+                    // 選択した写真がある際
+                    if let newValue {
+                        Task {
+                            // Data型で写真を取り出す
+                            if let data = try? await newValue.loadTransferable(type: Data.self) {
+                                // 写真がある際
+                                // 写真をcaptureImageに保存
+                                captureImage = UIImage(data: data)
+                            }
                         }
                     }
-                }
-            }) // .onChange End
-        } // VStack End
+                }) // .onChange End
+            } // VStack End
+        }
         // 撮影した写真を保持する状態変数が変化したら実行
         .onChange(of: captureImage, initial: true, { oldValue, newValue in
             if let _ = newValue {
@@ -105,6 +123,7 @@ struct ContentView: View {
         }) // .onChange End
         
     } // body End
+
 } // ContentView End
 
 #Preview {
--- PhotoCam/EffectView.swift
+++ PhotoCam/EffectView.swift
@@ -38,120 +38,140 @@ struct EffectView: View {
     ]
     
     // 選択中のエフェクト (filterArrayの添字)
-    @State var filterSelection = 0
+    @State var filterSelectNumber = 0
     
     var body: some View {
-        // 縦方向レイアウト
-        VStack {
-            // スペース追加
-            Spacer()
-            
-            if let showImage {
-                //表示する写真がある場合は画面表示
-                Image(uiImage: showImage)
-                // リサイズ
-                    .resizable()
-                // アスペクト比（縦横比）を維持し画面内に収める
-                    .scaledToFit()
-            } // if End
-            // スペース追加
-            Spacer()
-            // 「エフェクト」ボタン
-            Button {
-                // ボタンタップアクション
-                // フィルタ名指定
-                let filterName = "CIPhotoEffectMono"
-                // 元々の画像の回転角度取得
-                let rotate = captureImage.imageOrientation
-                // UImage形式の画像をCIImage形式に変換
-                let inputImage = CIImage(image: captureImage)
+        ZStack {
+            // 背景画像を表示
+            Image(.screen)
+            // リサイズ
+                .resizable()
+            // セーフエリア外まで表示
+                .ignoresSafeArea()
+            // アスペクト比を維持し短辺基準に収める
+                .scaledToFill()
+            // 縦方向レイアウト
+            VStack {
+                // スペース追加
+                Spacer()
                 
-                // フィルタ名を指定しCIFilterインスタンス取得
-                guard let effectFilter = CIFilter(name: filterName) else {
-                    return
-                }
-                
-                // フィルタ加工のパラメータ初期化
-                effectFilter.setDefaults()
-                // インスタンスにフィルタ加工する元画像を設定
-                effectFilter.setValue(inputImage, forKey: kCIInputImageKey)
-                
-                guard let outputImage = effectFilter.outputImage else {
-                    return
-                }
-                
-                // CIContextのインスタンス取得
-                let ciContext = CIContext(options: nil)
-                // フィルタ加工後の画像をCIContext上に描画し、
-                guard let cgImage = ciContext.createCGImage(outputImage, from: outputImage.extent) else {
-                    return
-                }
-                
-                // フィルタ加工後の画像をCGImage形式から
-                showImage = UIImage(
-                    cgImage: cgImage,
-                    scale: 1,
-                    orientation: rotate
-                )
-                
-            } label: {
-                // テキスト表示
-                Text("エフェクト")
-                // 横幅いっぱい
-                .frame(maxWidth: .infinity)
-                // 高さ50ポイント指定
-                .frame(height: 50)
-                // 文字列センタリング指定
-                .multilineTextAlignment(.center)
-                // 背景に青色指定
-                .background(Color.blue)
-                // 文字色を白色指定
-                .foregroundColor(Color.white)
-            } // 「エフェクト」ボタン End
-            // 上下左右に余白追加
-            .padding()
-            
-            // showImageをアンラップ
-            if let showImage {
-                // shareImageから共有画像を生成
-                let shareImage = Image(uiImage: showImage)
-                // 共有シート
-                ShareLink(item: shareImage, subject: nil, message: nil, preview: SharePreview("Photo", image: shareImage)) {
+                if let showImage {
+                    //表示する写真がある場合は画面表示
+                    Image(uiImage: showImage)
+                    // リサイズ
+                        .resizable()
+                    // アスペクト比（縦横比）を維持し画面内に収める
+                        .scaledToFit()
+                } // if End
+                // スペース追加
+                Spacer()
+                // 「エフェクト」ボタン
+                Button {
+                    // ボタンタップアクション
+                    // フィルタ名指定
+                    let filterName = filterArray[filterSelectNumber]
+                    
+                    // 次回に適用するフィルタを決めておく
+                    filterSelectNumber += 1
+                    // 最後のフィルタまで適用した場合
+                    if filterSelectNumber == filterArray.count {
+                        // 最後の場合は、最初のフィルタに戻す
+                        filterSelectNumber = 0
+                    }
+                    // 元々の画像の回転角度取得
+                    let rotate = captureImage.imageOrientation
+                    // UImage形式の画像をCIImage形式に変換
+                    let inputImage = CIImage(image: captureImage)
+                    
+                    // フィルタ名を指定しCIFilterインスタンス取得
+                    guard let effectFilter = CIFilter(name: filterName) else {
+                        return
+                    }
+                    
+                    // フィルタ加工のパラメータ初期化
+                    effectFilter.setDefaults()
+                    // インスタンスにフィルタ加工する元画像を設定
+                    effectFilter.setValue(inputImage, forKey: kCIInputImageKey)
+                    
+                    guard let outputImage = effectFilter.outputImage else {
+                        return
+                    }
+                    
+                    // CIContextのインスタンス取得
+                    let ciContext = CIContext(options: nil)
+                    // フィルタ加工後の画像をCIContext上に描画し、
+                    guard let cgImage = ciContext.createCGImage(outputImage, from: outputImage.extent) else {
+                        return
+                    }
+                    
+                    // フィルタ加工後の画像をCGImage形式から
+                    showImage = UIImage(
+                        cgImage: cgImage,
+                        scale: 1,
+                        orientation: rotate
+                    )
+                    
+                } label: {
                     // テキスト表示
-                    Text("シェア")
+                    Text("エフェクト")
                     // 横幅いっぱい
                         .frame(maxWidth: .infinity)
                     // 高さ50ポイント指定
                         .frame(height: 50)
+                    // 文字列センタリング指定
+                        .multilineTextAlignment(.center)
+                    // 背景に青色指定
+                        .background(Color.blue)
+                    // 文字色を白色指定
+                        .foregroundColor(Color.white)
+                } // 「エフェクト」ボタン End
+                // 上下左右に余白追加
+                .padding()
+                
+                // showImageをアンラップ
+                if let showImage {
+                    // shareImageから共有画像を生成
+                    let shareImage = Image(uiImage: showImage)
+                    // 共有シート
+                    ShareLink(item: shareImage, subject: nil, message: nil, preview: SharePreview("Photo", image: shareImage)) {
+                        // テキスト表示
+                        Text("シェア")
+                        // 横幅いっぱい
+                            .frame(maxWidth: .infinity)
+                        // 高さ50ポイント指定
+                            .frame(height: 50)
+                        // 背景を青色指定
+                            .background(Color.blue)
+                        // 文字色を白色指定
+                            .foregroundStyle(Color.white)
+                    } // ShareLink End
+                    // 上下左右に余白追加
+                    .padding()
+                } // アンラップ End
+                
+                // 「閉じる」ボタン
+                Button {
+                    // ボタンタップアクション
+                    // エフェクト編集画面を閉じる
+                    isShowSheet.toggle()
+                } label: {
+                    // テキスト表示
+                    Text("閉じる")
+                    // 横幅いっぱい
+                        .frame(maxWidth: .infinity)
+                    // 高さ50ポイント指定
+                        .frame(height: 50)
+                    // 文字列センタリング指定
+                        .multilineTextAlignment(.center)
                     // 背景を青色指定
                         .background(Color.blue)
                     // 文字色を白色指定
                         .foregroundStyle(Color.white)
-                } // ShareLink End
+                } // 「閉じる」ボタン End
                 // 上下左右に余白追加
                 .padding()
-            } // アンラップ End
-            
-            // 「閉じる」ボタン
-            Button {
-                // ボタンタップアクション
-            } label: {
-                // テキスト表示
-                Text("閉じる")
-                // 横幅いっぱい
-                .frame(maxWidth: .infinity)
-                // 高さ50ポイント指定
-                    .frame(height: 50)
-                // 文字列センタリング指定
-                    .multilineTextAlignment(.center)
-                // 背景を青色指定
-                    .background(Color.blue)
-                // 文字色を白色指定
-                    .foregroundStyle(Color.white)
-            } // 「閉じる」ボタン End
-            // 上下左右に余白追加
-            .padding()
-        } // VStack End
+            } // VStack End
+        }
         .onAppear {
             // 撮影した写真を表示する写真に設定
             showImage = captureImage
